8c865b35dda981dc20244c7e37d8b50908aea5db
Supreeta03 <supreeta2003@gmail.com>
2024-03-01 15:50:54 +0530

    code cleaned up for load_llm()

--- LLM_mistral7b_setup.py
+++ LLM_mistral7b_setup.py
@@ -1,5 +1,4 @@
 import torch
-from ctransformers import AutoModelForCausalLM as cAutoModelForCausalLM
 from torch import cuda
 from transformers import pipeline, AutoTokenizer, BitsAndBytesConfig,AutoModelForCausalLM, AutoConfig
 from langchain_community.llms import HuggingFacePipeline
@@ -9,45 +8,31 @@ from langchain.chains import RetrievalQA
 def load_llm():
 
     device = 'cuda:0' if cuda.is_available() else 'cpu'
-    model_path_gpu = "mistralai/Mistral-7B-Instruct-v0.1"
-    model_path_cpu = "models/mistral-7b-instruct-v0.1.Q2_K.gguf"
 
     if device == "cpu":
-        print("Setting up model for cpu")
-        model = cAutoModelForCausalLM.from_pretrained(
-            model_path_cpu,
-            model_type="mistral",
-            gpu_layers=0,
-            hf=True
-        )
+        model_id = "models/mistral-7b-instruct-v0.1.Q2_K.gguf"
     else:
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True
-        )
-        print("Setting up model for gpu")
+        model_id = "mistralai/Mistral-7B-Instruct-v0.1"
 
-        model_config = AutoConfig.from_pretrained(model_path_gpu)
-        model = AutoModelForCausalLM.from_pretrained(
-            model_path_gpu,
-            trust_remote_code=True,
-            config=model_config,
-            quantization_config=bnb_config,
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 
-        )
-
-    tokenizer = AutoTokenizer.from_pretrained(model_path_gpu)
-
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True
+    )
+    model_config = AutoConfig.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+        config=model_config,
+        quantization_config=bnb_config,
+    )
 
     pipe = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
         torch_dtype=torch.bfloat16,
-        device_map="auto",
-        max_new_tokens=500,
-        repetition_penalty=1.1,
-        eos_token_id=tokenizer.eos_token_id,
-        pad_token_id=tokenizer.eos_token_id,
+        max_new_tokens=500
     )
 
     llm = HuggingFacePipeline(pipeline=pipe)
@@ -56,7 +41,6 @@ def load_llm():
 
 def retriever_engine(llm, docsearch):
     retriever = docsearch.as_retriever(search_type="mmr", search_kwargs={"k": 4})
-    # create a retrieval QA Chain
     QnA = RetrievalQA.from_chain_type(
         llm,
         retriever=retriever,
